44d9f37e17bb36306655f2f076cdc0b9c68262e2
Radhi hudijan <radhi@digifit.eu>
2023-07-18 18:13:14 +0200

    add props

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -2,7 +2,10 @@
     <div>
         <h2>i love you</h2>
         <ul>
-            <friend-contact></friend-contact>
+            <friend-contact v-for='friend in friends' :key="friend.id" :id="friend.id" :name="friend.name"
+                :phone-number="friend.phone" :email-address="friend.email" :isFavorite="friend.isFavorite"
+                @toggle-favorite="toggleFavoriteStatus">
+            </friend-contact>
         </ul>
     </div>
 </template>
@@ -16,9 +19,24 @@ export default {
                     id: 'radhi',
                     name: 'radhi hudijan',
                     phone: "012238070",
-                    email: "radhi@fff.com"
+                    email: "radhi@fff.com",
+                    isFavorite: true
+                },
+                {
+                    id: 'Ali',
+                    name: 'Ali hudijan',
+                    phone: "0222222",
+                    email: "Ali@fff.com",
+                    isFavorite: false
+
                 }
             ]
+        }
+    },
+    methods: {
+        toggleFavoriteStatus (friendID) {
+            const toggledFriend = this.friends.find(friend => friend.id === friendID)
+            toggledFriend.isFavorite = !toggledFriend.isFavorite
         }
     }
 }
@@ -26,66 +44,66 @@ export default {
 
 <style>
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 
 html {
-  font-family: 'Jost', sans-serif;
+    font-family: 'Jost', sans-serif;
 }
 
 body {
-  margin: 0;
+    margin: 0;
 }
 
 header {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  background-color: #58004d;
-  color: white;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 3rem auto;
+    border-radius: 10px;
+    padding: 1rem;
+    background-color: #58004d;
+    color: white;
+    text-align: center;
+    width: 90%;
+    max-width: 40rem;
 }
 
 #app ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
+    margin: 0;
+    padding: 0;
+    list-style: none;
 }
 
 #app li {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 1rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 1rem auto;
+    border-radius: 10px;
+    padding: 1rem;
+    text-align: center;
+    width: 90%;
+    max-width: 40rem;
 }
 
 #app h2 {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #58004d;
-  margin: 0 0 1rem 0;
+    font-size: 2rem;
+    border-bottom: 4px solid #ccc;
+    color: #58004d;
+    margin: 0 0 1rem 0;
 }
 
 #app button {
-  font: inherit;
-  cursor: pointer;
-  border: 1px solid #ff0077;
-  background-color: #ff0077;
-  color: white;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+    font: inherit;
+    cursor: pointer;
+    border: 1px solid #ff0077;
+    background-color: #ff0077;
+    color: white;
+    padding: 0.05rem 1rem;
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
 
 #app button:hover,
 #app button:active {
-  background-color: #ec3169;
-  border-color: #ec3169;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+    background-color: #ec3169;
+    border-color: #ec3169;
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
 </style>
--- vue-test/src/components/FriendContact.vue
+++ vue-test/src/components/FriendContact.vue
@@ -1,30 +1,58 @@
 <template>
     <li>
-        <h2>{{ friend.name }}</h2>
+        <h2>{{ name }} {{ isFavorite ? "(Favorite)" : '' }}</h2>
         <button @click="toggleDetails"> {{ detailsAreVisible ? 'Hide Details' : 'Show Details' }}</button>
+        <button @click="toggleFavorite"> Toggle Favorite</button>
+
         <ul v-if="detailsAreVisible">
-            <li><strong>Phone: </strong>{{ friend.phone }}</li>
-            <li><strong>Email: </strong>{{ friend.email }}</li>
+            <li><strong>Phone: </strong>{{ phoneNumber }}</li>
+            <li><strong>Email: </strong>{{ emailAddress }}</li>
         </ul>
     </li>
 </template>
 
 <script>
 export default {
+    // props: ['name', 'phoneNumber', 'emailAddress','isFavorite'],
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true
+        },
+        emailAddress: {
+            type: String,
+            required: true
+        },
+        isFavorite: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+
+    },
+    emits: ['toggle-favorite'],
     data () {
         return {
             detailsAreVisible: false,
-            friend: {
-                id: 'radhi',
-                name: 'radhi hudijan',
-                phone: "012238070",
-                email: "radhi@fff.com"
-            }
+
         }
     },
     methods: {
         toggleDetails () {
             this.detailsAreVisible = !this.detailsAreVisible
+        },
+        toggleFavorite () {
+            // this.friendIsFavorite =!this.friendIsFavorite
+            this.$emit('toggle-favorite', this.id)
+            console.log(`inside${this.id}`)
         }
     },
 }
